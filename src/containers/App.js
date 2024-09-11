087aa75cf5e67434d2693c1984ba3d5370a113e3
Oleg Romanenko <oleginga@gmail.com>
2024-09-11 18:38:49 +0200

    finished app

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,12 +9,14 @@ import './App.css';
 function App() {
   const [robots, setRobots] = useState([]);
   const [searchfield, setSearchfield] = useState('');
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => {setRobots(users)});
-  }, []);
+      console.log(count);
+  }, [count]); // only run if count changes
 
   const onSearchChange = (event) => {
     setSearchfield(event.target.value);
@@ -28,6 +30,7 @@ function App() {
       (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
+          <button onClick={() => setCount(count+1)}>Click Me!</button>
           <SearchBox searchChange={onSearchChange}/>
           <Scroll>
             <ErrorBoundry>
